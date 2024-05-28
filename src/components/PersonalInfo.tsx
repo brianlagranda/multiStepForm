15e7e645f80f0f2938e0f26f99e81068cd734655
Brian Lagranda <brianlagranda@gmail.com>
2024-05-28 17:43:53 -0300

    feat: add first approach personalInfo form

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -10,21 +10,63 @@ const PersonalInfo = () => {
     } = useForm<Inputs>();
     const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-    console.log(watch('example'));
+    console.log(watch('name'));
 
     return (
-        <div className=' bg-red-200 w-full h-full'>
-            <h1>Personal Info</h1>
-            <p>Please provide your name, email address, and phone number.</p>
+        <div className=' bg-neutral-white w-full h-full rounded-lg grid grid-cols-2 p-4'>
+            <aside className='col-start-1 w-1/3 bg-primary-light-blue rounded-lg'>
+                Step
+                <p className='text-black'>1</p>
+                <p className='text-black'>2</p>
+                <p className='text-black'>3</p>
+                <p className='text-black'>4</p>
+            </aside>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input defaultValue='test' {...register('example')} />
+            <div className='col-start-2'>
+                <h1>Personal Info</h1>
+                <p>
+                    Please provide your name, email address, and phone number.
+                </p>
 
-                <input {...register('exampleRequired', { required: true })} />
-                {errors.exampleRequired && <span>This field is required</span>}
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className='flex flex-col'
+                >
+                    <input
+                        defaultValue={''}
+                        placeholder='e.g. Stephen King'
+                        {...register('name', { required: true })}
+                    />
+                    {errors.name && (
+                        <span className='text-primary-strawberry-red font-bold'>
+                            This field is required
+                        </span>
+                    )}
 
-                <input type='submit' />
-            </form>
+                    <input
+                        defaultValue={''}
+                        placeholder='e.g. stephenking@lorem.com'
+                        {...register('email', { required: true })}
+                    />
+                    {errors.email && (
+                        <span className='text-primary-strawberry-red font-bold'>
+                            This field is required
+                        </span>
+                    )}
+
+                    <input
+                        defaultValue={''}
+                        placeholder='e.g. +1 234 567 890'
+                        {...register('phone')}
+                    />
+
+                    <input
+                        type='submit'
+                        className='w-1/4 h-10 rounded-lg bg-primary-marine-blue hover:bg-primary-pastel-blue'
+                        name='Next Step'
+                    />
+                </form>
+            </div>
         </div>
     );
 };
