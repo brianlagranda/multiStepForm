70e6df8576f4bf3d3f457b76f1ca888856b9b4bb
Brian Lagranda <brianlagranda@gmail.com>
2024-07-17 13:16:07 -0300

    feat: add redux-persist to storage data

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import formReducer from '../features/form/formSlice';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, formReducer);
 
 const store = configureStore({
     reducer: {
-        form: formReducer,
+        form: persistedReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
